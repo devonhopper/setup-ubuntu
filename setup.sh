d41abc7704221b9b534c677c6302210bf24013d9
Devon Hopper <github@diamanttech.io>
2020-06-12 01:50:25 -0700

    Replaced echo with printf

--- setup.sh
+++ setup.sh
@@ -1,38 +1,38 @@
 # Ensure Ubuntu is up to date
-echo "\n$(tput setaf 1)Updating Ubuntu...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Updating Ubuntu...$(tput sgr 0)\n"
 sudo apt update
-echo "\n"
+printf "\n"
 sudo apt upgrade -y
 
 # Install some needed tools
-echo "\n$(tput setaf 1)Install Tree...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Install Tree...$(tput sgr 0)\n"
 sudo apt install tree -y
-echo "\n$(tput setaf 1)Install FD...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Install FD...$(tput sgr 0)\n"
 sudo apt install fd-find -y
-echo "\n$(tput setaf 1)Install Master Commander...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Install Master Commander...$(tput sgr 0)\n"
 sudo apt install mc -y
 
 # Install Zsh
-echo "\n$(tput setaf 1)Install Zsh...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Install Zsh...$(tput sgr 0)\n"
 sudo apt install zsh -y
 sudo usermod -s /usr/bin/zsh $(whoami)
 
 # Install Antigen
-echo "\n$(tput setaf 1)Install Antigen...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Install Antigen...$(tput sgr 0)\n"
 curl -L git.io/antigen > ~/.antigen.zsh
 
 # Install Nord dircolors
-echo "\n$(tput setaf 1)Install Nord dircolors...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Install Nord dircolors...$(tput sgr 0)\n"
 git clone https://github.com/arcticicestudio/nord-dircolors ~/nord-dircolors
 cp ~/nord-dircolors/src/dir_colors ~/.dir_colors
 
 # Copy zshrc and p10k.zsh
-echo "\n$(tput setaf 1)Customizing...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Customizing...$(tput sgr 0)\n"
 cp zshrc ~/.zshrc
 cp p10k.zsh ~/.p10k.zsh
 
 # Execute Zsh
-echo "\n$(tput setaf 1)Launching Zsh...$(tput sgr 0)"
+printf "\n$(tput setaf 1)Launching Zsh...$(tput sgr 0)\n"
 zsh
 
 
